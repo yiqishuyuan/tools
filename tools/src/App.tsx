fb6393db49e8f2965cb4db5ef80dc4a69b68d7c9
lily <falaonline@outlook.com>
2026-03-19 04:40:49 +0800

    add:a

--- tools/src/App.tsx
+++ tools/src/App.tsx
@@ -1,15 +1,19 @@
-import { HelmetProvider } from 'react-helmet-async'
-import AppRouter from './router'
-import { ThemeModeProvider } from './context/ThemeModeContext'
+import { HelmetProvider } from "react-helmet-async";
+import AppRouter from "./router";
+import { ThemeModeProvider } from "./context/ThemeModeContext";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <HelmetProvider>
-      <ThemeModeProvider>
-        <AppRouter />
-      </ThemeModeProvider>
-    </HelmetProvider>
-  )
+    <>
+      <HelmetProvider>
+        <ThemeModeProvider>
+          <AppRouter />
+        </ThemeModeProvider>
+      </HelmetProvider>
+      <Analytics />
+    </>
+  );
 }
 
-export default App
+export default App;
